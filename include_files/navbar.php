d1140372e42e19715291588a752f175c6eaa6f99
gkchef69 <77124050+gkchef69@users.noreply.github.com>
2023-03-25 19:43:47 +0200

    Add files via upload

---
+++ include_files/navbar.php
@@ -0,0 +1,34 @@
+
+<!-- navbar -->
+<div class="container">
+    <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="../convert__units/index.php"><img src="https://i.ibb.co/vLDyPtM/ak-logo-yellow.png" alt="logo"></a>
+            <button class="navbar-toggler collapsed d-flex d-lg-none flex-column justify-content-around" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="toggler-icon top-bar"></span>
+                <span class="toggler-icon middle-bar"></span>
+                <span class="toggler-icon bottom-bar"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="../convert__units/index.php">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./add_item.php">Add Items</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./convert_units.php">Convert Units</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./edit_item.php">Edit Item</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="./remove_item.php">Delete Item</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    
+</div>
